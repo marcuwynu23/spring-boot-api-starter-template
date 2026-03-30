9ca1e41aae3a02c3af38da55325abfc3e4e37f0c
Mark Wayne Menorca <marcuwynu23@gmail.com>
2026-03-30 21:03:36 +0800

    refactor: replace SampleItem with TodoItem model and repository, update API endpoints in TodoController for CRUD operations

--- src/main/java/com/marcuwynu23/repositories/SampleItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.marcuwynu23.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcuwynu23.models.SampleItem;
-
-public interface SampleItemRepository extends JpaRepository<SampleItem, Long> {
-}
